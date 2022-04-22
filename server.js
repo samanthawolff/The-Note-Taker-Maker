c2ad39c1d5c54ba07435289e6b4ae6d7a9312fe0
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-21 21:21:30 -0500

    linking html files together

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mySQL = require('mysql');
+const fs = require('fs');
+const path = require('path');
 
 
 const app = express();
@@ -8,6 +10,15 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 
 app.listen(PORT, () => {
