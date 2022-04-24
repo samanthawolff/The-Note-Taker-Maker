67e3256d3f4e88056ae3e68ea72d87a67def4eeb
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-23 21:01:22 -0500

    attempted delete route and add commenting

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
+// Dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 
+// Sets up express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+// Link to assets and setting up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
 
-    var notes = JSON.parse(data);
-
-    app.get('/api/notes', function (req, res) {
-        res.json(notes);
-    });
-
-    app.post('/api/notes', function (req, res) {
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDatabase();
-        return console.log('New Note Added!');
-    });
-})
-
-
+// link HTML files together (HTML routes)
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -38,6 +25,34 @@ app.get('/notes', function (req, res) {
 });
 
 
+// API routes
+fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    var notes = JSON.parse(data);
+
+    // GET request
+    app.get('/api/notes', function (req, res) {
+        res.json(notes);
+    });
+
+    // POST request
+    app.post('/api/notes', function (req, res) {
+        let newNote = req.body;
+        notes.push(newNote);
+        updateDatabase();
+        console.log('New Note Added!');
+    });
+
+    // My attempt at a DELETE request (it only somewhat works :/ )
+    app.delete('/api/notes/:id', function (req, res) {
+        notes.splice(req.params.id, 1);
+        updateDatabase();
+        console.log('Your note was deleted!')
+    });
+});
+
+// Set up listener
 app.listen(PORT, () => {
     console.log('API server is ready on port ${PORT}')
 });
